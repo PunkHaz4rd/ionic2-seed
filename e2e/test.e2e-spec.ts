ac0ad187b0defaf1f489c3e6b6e64e93d25306e0
Romain FOURNEREAU <r.fournereau@groupe-creative.fr>
2017-07-27 10:54:15 +0200

    fix(setup): added conf files for protractor

---
+++ e2e/test.e2e-spec.ts
@@ -0,0 +1,11 @@
+import { browser, element, by, ElementFinder } from 'protractor';
+ 
+describe('Example E2E Test', () => {
+ 
+  beforeEach(() => {
+    browser.get('');
+  });
+ 
+  // Writing Tests
+
+});
